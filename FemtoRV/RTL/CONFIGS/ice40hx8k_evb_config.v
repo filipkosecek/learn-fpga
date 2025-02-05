a312fe6e585db0763127b610150d0faae86c4cdd
Filip Kosecek <filip.kosecek@gmail.com>
2025-02-05 17:12:14 +0100

    Modify config values
    Increase the clock frequency, align the code
    section beginning with flash memory block size
    and increase RAM size

--- FemtoRV/RTL/CONFIGS/ice40hx8k_evb_config.v
+++ FemtoRV/RTL/CONFIGS/ice40hx8k_evb_config.v
@@ -1,9 +1,8 @@
-// Default femtosoc configuration file for IceStick
+// Default femtosoc configuration file for ICE40HX8K-EVB
 
 /************************* Devices **********************************************************************************/
 
 `define NRV_IO_LEDS          // Mapped IO, LEDs D1,D2,D3,D4 (D5 is used to display errors)
-//`define NRV_IO_IRDA          // In IO_LEDS, support for the IRDA on the IceStick (WIP)
 `define NRV_IO_UART          // Mapped IO, virtual UART (USB)
 //`define NRV_IO_SSD1351       // Mapped IO, 128x128x64K OLED screen
 //`define NRV_IO_MAX7219       // Mapped IO, 8x8 led matrix
@@ -12,16 +11,17 @@
 /************************* Processor configuration *******************************************************************/
 
 `define NRV_FEMTORV32_QUARK
-`define NRV_FREQ 20                 // Validated at 50 MHz on the IceStick. Can overclock to 70 MHz.
-`define NRV_RESET_ADDR 32'h00821500 // Jump execution to SPI Flash (800000h, +128k(20000h) for FPGA bitstream)
+`define NRV_FREQ 40
+`define NRV_RESET_ADDR 32'h00830000 // Jump execution to SPI Flash (800000h, +192k(30000h) for FPGA bitstream)
+                                    // Maximum bitstream size is 136448 bytes and the firmware address is
+				    // aligned to the block size 64KB, hence 192KB.
 `define NRV_COUNTER_WIDTH 24        // Number of bits in cycles counter
-//`define NRV_TWOLEVEL_SHIFTER        // Faster shifts
-// tinyraytracer: 70 MHz, 17:30 
+`define NRV_TWOLEVEL_SHIFTER        // Faster shifts
 
 
 /************************* RAM (in bytes, needs to be a multiple of 4)***********************************************/
 
-`define NRV_RAM 8192
+`define NRV_RAM 10240
 
 
 /************************* Advanced devices configuration ***********************************************************/
