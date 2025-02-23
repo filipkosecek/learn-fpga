fd9c79104993061cd2e3e06ae8ae77b797b3a3b4
Filip Kosecek <filip.kosecek@gmail.com>
2025-02-23 19:48:01 +0100

    FIRMWARE/DEMO/build.sh: add random string test

--- FemtoRV/FIRMWARE/DEMO/build.sh
+++ FemtoRV/FIRMWARE/DEMO/build.sh
@@ -25,21 +25,37 @@ genstr () {
 	echo "volatile const char str[] = \"${STR}\";" > target_string.h
 }
 
+genstr_rand () {
+	echo -n "volatile const char str[] = {" > target_string.h
+	head -c $1 /dev/urandom | xxd -i >> target_string.h
+	# append a null byte in case there was no in the stream
+	for i in {1..32}; do
+		C_HEADER="${C_HEADER}, 0x00"
+	done
+	echo "${C_HEADER}};" >> target_string.h
+}
+
 build () {
 	rm -f main.hex
 	make RVUSERCFLAGS="-DSTRINGA${LENGTH} -DSTRLEN_VECTORIZED${CHUNK}" main.hex
 	replace_nops
 }
 
-if [[ $# -eq 2 ]]; then
+ISRANDOM=
+if [[ $# -eq 3 ]]; then
 	LENGTH=$1
 	REGCOUNT=$2
 	CHUNK=$(($REGCOUNT * 4))
+	ISRANDOM=$3
 fi
 echo "String's length set to ${LENGTH}."
 echo "Chunk size set to ${CHUNK}."
 
-genstr $LENGTH
-build
+if [[ -n $ISRANDOM ]]; then
+	genstr_rand $LENGTH
+else
+	genstr $LENGTH
+fi
 
+build
 make -C ../../ BENCH.icarus
