c4a2fc2bd4c341809fdf0e4c1ad4cfc0f20e04cd
Filip Kosecek <filip.kosecek@gmail.com>
2025-03-02 11:26:17 +0100

    FIRMWARE/DEMO: enhance the build script

--- FemtoRV/FIRMWARE/DEMO/build.sh
+++ FemtoRV/FIRMWARE/DEMO/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LENGTH=1025
+MAXREGCOUNT=8
 REGCOUNT=8
 CHUNK=$(($REGCOUNT * 4))
 
@@ -35,22 +36,27 @@ genstr_rand () {
 	echo "${C_HEADER}};" >> target_string.h
 }
 
-build () {
+build_program () {
 	rm -f main.hex
 	make RVUSERCFLAGS="-DSTRINGA${LENGTH} -DSTRLEN_VECTORIZED${CHUNK}" main.hex
 	replace_nops
 }
 
+setup_CPU () {
+	sed -i "s/\\(parameter SIMD_REG_COUNT = \\)\\([2-8]\\);/\\1${MAXREGCOUNT};/g" ../../RTL/PROCESSOR/femtorv32_quark_simd.v
+}
+
 ISRANDOM=
-if [[ $# -lt 2 ]] || [[ $# -gt 3 ]]; then
+if [[ $# -lt 3 ]] || [[ $# -gt 4 ]]; then
 	echo "Wrong argument count." 1>&2
 	exit 1
 else
 	LENGTH=$1
-	REGCOUNT=$2
+	MAXREGCOUNT=$2
+	REGCOUNT=$3
 	CHUNK=$(($REGCOUNT * 4))
-	if [[ $# -eq 3 ]]; then
-		ISRANDOM=$3
+	if [[ $# -eq 4 ]]; then
+		ISRANDOM=$4
 	fi
 fi
 echo "String's length set to ${LENGTH}."
@@ -62,5 +68,6 @@ else
 	genstr $LENGTH
 fi
 
-build
+setup_CPU
+build_program
 make -C ../../ BENCH.icarus
