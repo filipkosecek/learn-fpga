a38635bbf1535e3445ca7ca0df4c5abe2f5e38dd
Filip Kosecek <filip.kosecek@gmail.com>
2025-04-12 11:10:07 +0200

    processor: make the register mappings more readable

--- FemtoRV/RTL/PROCESSOR/femtorv32_quark_vec.v
+++ FemtoRV/RTL/PROCESSOR/femtorv32_quark_vec.v
@@ -128,7 +128,26 @@ module FemtoRV32(
         end
     end
 
-`ifndef VECTOR_BLOCK_BRAM
+`ifdef VECTOR_BLOCK_BRAM
+
+    // This setup infers to 16 bram blocks for some reason
+    // unlike the original one.
+    // To work with this, change RAM size to 8192 bytes
+    // in RTL/CONFIG/ice40hx8k_evb_config.v, FIRMWARE/CRT/spiflash_ice40hx8k_evb.ld
+    // and the assembly code loading the registers.
+    localparam [5 * 8 - 1 : 0] vecRegId = {
+       5'd16,
+       5'd15,
+       5'd30,
+       5'd29,
+       5'd6,
+       5'd5,
+       5'd7,
+       5'd31
+    };
+
+`else
+
     // Mapping rs -> rv32i register index
     localparam [5 * 8 - 1 : 0] vecRegId = {
        5'd31,
@@ -141,22 +160,6 @@ module FemtoRV32(
        5'd17
     };
 
-`else
-    // This setup infers to 16 bram blocks for some reason
-    // unlike the original one.
-    // To work with this, change RAM size to 8192 bytes
-    // in RTL/CONFIG/ice40hx8k_evb_config.v, FIRMWARE/CRT/spiflash_ice40hx8k_evb.ld
-    // and the assembly code loading the registers.
-    localparam [5 * 8 - 1 : 0] vecRegId = {
-        5'd16,
-        5'd15,
-        5'd30,
-        5'd29,
-        5'd6,
-        5'd5,
-        5'd7,
-        5'd31
-    };
 `endif
 
     wire isMultiCmp = isMultiCmpReg | isMultiCmpImm;
