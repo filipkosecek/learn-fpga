14ba658eaa7aa7dbced7d73a3e0ba66ecb80df9b
Filip Kosecek <filip.kosecek@gmail.com>
2025-05-09 09:40:07 +0200

    ice40hx8k_evb: change the default frequency to 48 MHz

--- FemtoRV/RTL/CONFIGS/ice40hx8k_evb_config.v
+++ FemtoRV/RTL/CONFIGS/ice40hx8k_evb_config.v
@@ -12,7 +12,7 @@
 
 `define NRV_FEMTORV32_QUARK_VEC
 `define VECTOR_FORCE_BRAM           // Forces Yosys to infer the register file to block RAM
-`define NRV_FREQ 40
+`define NRV_FREQ 48
 `define NRV_RESET_ADDR 32'h00830000 // Jump execution to SPI Flash (800000h, +192k(30000h) for FPGA bitstream)
                                     // Maximum bitstream size is 136448 bytes and the firmware address is
 				    // aligned to the block size 64KB, hence 192KB.
